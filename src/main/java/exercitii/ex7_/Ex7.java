04702c37a02ba9697991b63ed827942c40ab7119
Carminamalia <carminamalia70@gmail.com>
2023-09-24 13:11:50 +0300

    add ex 7 rezolvat

--- src/main/java/exercitii/ex7_/Ex7.java
+++ src/main/java/exercitii/ex7_/Ex7.java
@@ -1,7 +1,9 @@
 package exercitii.ex7_;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Ex7 {
     public static void main(String[] args) {
@@ -11,6 +13,20 @@ public class Ex7 {
 //Input: {“ana”, “are”, “mere”, “are”}
 //Output: {ana=1, are=2, mere=1}
         List<String> words = Arrays.asList("ana","are","mere","are");
-
+        System.out.println(groupWordsByFrecvency(words));
+    }
+    public static Map<String,Integer> groupWordsByFrecvency(List<String> words){
+        Map<String,Integer> wordsByFrecvency = new HashMap<>();
+        //parcurg lista si, pt fiecare elem:
+          //daca cheia este deja in lista
+             //cresc valoarea dela acea cheie cu e
+          //altfel,daca cheia nu este ,adaug cheia cu valoarea 1
+        for (String word:words){
+            if (wordsByFrecvency.containsKey(word)){
+                wordsByFrecvency.put(word,wordsByFrecvency.get(word)+1);
+            }else {
+                wordsByFrecvency.put(word,1);
+            }
+        }return wordsByFrecvency;
     }
 }
